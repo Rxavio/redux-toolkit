02b01a0d5fe2493de6e57fbe5e297d6426a346da
xavioo <rucahoxavi@gmail.com>
2022-01-16 09:19:29 +0200

    add to do form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from "react";
-import "./App.css";
-import Profile from "./components/Profile";
-import Login from "./components/Login";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddTodoForm from './components/AddTodoForm';
 
-function App() {
-  return (
-    <div className="App">
-      <Profile />
-      <Login />
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className='container bg-white p-4 mt-5'>
+			<h1>My Todo List</h1>
+			<AddTodoForm />
+
+		</div>
+	);
+};
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import userReducer from "./features/user";
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
-ReactDOM.render(
-  <React.StrictMode>
-      <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-reportWebVitals();
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
